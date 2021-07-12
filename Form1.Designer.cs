fb947f9864392a3a4736f35fe989b5f36ba69aaf
cheenaze <oqaymoments@gmail.com>
2021-07-12 18:32:54 +0300

    many improvements

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,42 +28,149 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.Main_b = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.setSecondFile_b = new System.Windows.Forms.Button();
+            this.setFirstFile_b = new System.Windows.Forms.Button();
+            this.setOutput_b = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
-            // button1
+            // Main_b
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.Font = new System.Drawing.Font("SimSun", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(202, 68);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "CONVERT TO BYTES";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Main_b.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.Main_b.FlatAppearance.BorderSize = 0;
+            this.Main_b.Font = new System.Drawing.Font("JetBrains Mono ExtraBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Main_b.Location = new System.Drawing.Point(12, 12);
+            this.Main_b.Name = "Main_b";
+            this.Main_b.Size = new System.Drawing.Size(202, 68);
+            this.Main_b.TabIndex = 0;
+            this.Main_b.Text = "CONVERT TO BYTES";
+            this.Main_b.UseVisualStyleBackColor = false;
+            this.Main_b.Click += new System.EventHandler(this.Main_b_Click);
             // 
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Font = new System.Drawing.Font("SimSun", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(12, 86);
+            this.textBox1.Font = new System.Drawing.Font("JetBrains Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(12, 198);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(202, 22);
             this.textBox1.TabIndex = 1;
+            // 
+            // setSecondFile_b
+            // 
+            this.setSecondFile_b.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.setSecondFile_b.FlatAppearance.BorderSize = 0;
+            this.setSecondFile_b.Font = new System.Drawing.Font("JetBrains Mono", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.setSecondFile_b.Location = new System.Drawing.Point(115, 86);
+            this.setSecondFile_b.Name = "setSecondFile_b";
+            this.setSecondFile_b.Size = new System.Drawing.Size(99, 22);
+            this.setSecondFile_b.TabIndex = 3;
+            this.setSecondFile_b.Text = "set 2nd file";
+            this.setSecondFile_b.UseVisualStyleBackColor = false;
+            this.setSecondFile_b.Click += new System.EventHandler(this.setSecondFile_b_Click);
+            // 
+            // setFirstFile_b
+            // 
+            this.setFirstFile_b.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.setFirstFile_b.FlatAppearance.BorderSize = 0;
+            this.setFirstFile_b.Font = new System.Drawing.Font("JetBrains Mono", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.setFirstFile_b.Location = new System.Drawing.Point(12, 86);
+            this.setFirstFile_b.Name = "setFirstFile_b";
+            this.setFirstFile_b.Size = new System.Drawing.Size(97, 22);
+            this.setFirstFile_b.TabIndex = 4;
+            this.setFirstFile_b.Text = "set 1st file";
+            this.setFirstFile_b.UseVisualStyleBackColor = false;
+            this.setFirstFile_b.Click += new System.EventHandler(this.setFirstFile_b_Click);
+            // 
+            // setOutput_b
+            // 
+            this.setOutput_b.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.setOutput_b.FlatAppearance.BorderSize = 0;
+            this.setOutput_b.Font = new System.Drawing.Font("JetBrains Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.setOutput_b.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.setOutput_b.Location = new System.Drawing.Point(12, 114);
+            this.setOutput_b.Name = "setOutput_b";
+            this.setOutput_b.Size = new System.Drawing.Size(202, 30);
+            this.setOutput_b.TabIndex = 5;
+            this.setOutput_b.Text = "set output";
+            this.setOutput_b.UseVisualStyleBackColor = false;
+            this.setOutput_b.Click += new System.EventHandler(this.setOutput_b_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("JetBrains Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.radioButton1.Location = new System.Drawing.Point(12, 150);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(53, 18);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "file";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("JetBrains Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.radioButton2.Location = new System.Drawing.Point(115, 150);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(53, 18);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = ".txt";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("JetBrains Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.radioButton3.Location = new System.Drawing.Point(12, 174);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(53, 18);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = ".cpp";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("JetBrains Mono", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.radioButton4.Location = new System.Drawing.Point(115, 174);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(39, 18);
+            this.radioButton4.TabIndex = 9;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = ".c";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.WindowFrame;
-            this.ClientSize = new System.Drawing.Size(226, 120);
+            this.ClientSize = new System.Drawing.Size(226, 232);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.setOutput_b);
+            this.Controls.Add(this.setFirstFile_b);
+            this.Controls.Add(this.setSecondFile_b);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Main_b);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.Text = "DLLtoHEX";
@@ -74,8 +181,15 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Main_b;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button setSecondFile_b;
+        private System.Windows.Forms.Button setFirstFile_b;
+        private System.Windows.Forms.Button setOutput_b;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -9,52 +9,118 @@ namespace DLLtoHEX
         public Form1()
         {
             InitializeComponent();
+            textBox1.Text = "Output file size: ";
         }
 
-        string dllPath;
+        string dllPath, firstfile, secondfile, outputFolder, FileFormat, outputFile = "DLLtoHEX";
+        string[] Formats = { "", ".txt", ".cpp", ".c" };
 
-        readonly string firstfile = "";
-        readonly string secondfile = ""; //it was necessary for me
-
-        readonly string output = ""; //YOUR PATH
-
-        private void button1_Click(object sender, EventArgs e)
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
-            OpenFileDialog openDialog = new OpenFileDialog
+            FileFormat = Formats[0];
+        }
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            FileFormat = Formats[1];
+        }
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            FileFormat = Formats[2];
+        }
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            FileFormat = Formats[3];
+        }
+
+
+        private void setFirstFile_b_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog
             {
-                Filter = "DLL|*.dll;"
+                Filter = "TXT|*.txt;"
             };
 
-            if (openDialog.ShowDialog(this) == DialogResult.OK)
+            if (open.ShowDialog(this) == DialogResult.OK)
             {
-                dllPath = openDialog.FileName;
+                setFirstFile_b.ForeColor = System.Drawing.Color.Green;
+                setFirstFile_b.Text = "file setted";
+                firstfile = open.FileName;
             }
+        }
 
-            byte[] hex = File.ReadAllBytes(dllPath);
-            File.WriteAllText(output, String.Empty);
-            using (StreamWriter sw = new StreamWriter(output))
+        private void setSecondFile_b_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog open = new OpenFileDialog
             {
-                sw.Write(File.ReadAllText(firstfile));
-                for (int x = 0; x < hex.Length; x++)
+                Filter = "TXT|*.txt;"
+            };
+
+            if (open.ShowDialog(this) == DialogResult.OK)
+            {
+                setSecondFile_b.ForeColor = System.Drawing.Color.Green;
+                setSecondFile_b.Text = "file setted";
+                secondfile = open.FileName;
+            }
+        }
+
+        private void setOutput_b_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog open = new FolderBrowserDialog();
+            if (open.ShowDialog(this) == DialogResult.OK)
+            {
+                setOutput_b.ForeColor = System.Drawing.Color.Green;
+                setOutput_b.Text = "output setted";
+                outputFolder = open.SelectedPath;
+            }
+        }
+
+
+        private void Main_b_Click(object sender, EventArgs e)
+        {
+            if (firstfile.Length != 0 && secondfile.Length != 0 && outputFolder.Length != 0)
+            {
+                OpenFileDialog openDialog = new OpenFileDialog
                 {
-                    sw.Write("0x");
-                    sw.Write("{0:X2}", hex[x]);
-                    sw.Write(", ");
-                    if (((x + 1) % 16) == 0)
-                        sw.WriteLine();
+                    Filter = "DLL|*.dll;"
+                };
+
+                if (openDialog.ShowDialog(this) == DialogResult.OK)
+                {
+                    dllPath = openDialog.FileName;
                 }
-                sw.Write(File.ReadAllText(secondfile));
+                outputFile = outputFolder + "\\" + outputFile + FileFormat;
+                MessageBox.Show(outputFile, "");
+                byte[] hex = File.ReadAllBytes(dllPath);
+                File.WriteAllText(outputFile, String.Empty);
+                using (StreamWriter sw = new StreamWriter(outputFile))
+                {
+                    sw.Write(File.ReadAllText(firstfile));
+                    for (int x = 0; x < hex.Length; x++)
+                    {
+                        sw.Write("0x");
+                        sw.Write("{0:X2}", hex[x]);
+                        sw.Write(", ");
+                        if (((x + 1) % 16) == 0)
+                            sw.WriteLine();
+                    }
+                    sw.Write(File.ReadAllText(secondfile));
+                }
+
+                FileInfo sizebyte = new FileInfo(outputFile);
+                long size_kbyte = sizebyte.Length / 1024;
+                textBox1.Text = ("Output file size: " + size_kbyte.ToString() + " Kb");
+
+                DialogResult result = MessageBox.Show("Exit?", "DLLtoHEX", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    Application.Exit();
+                }
             }
-
-            FileInfo sizebyte = new FileInfo(output);
-            long size_kbyte = sizebyte.Length / 1024;
-            textBox1.Text = (size_kbyte.ToString() + " Kb");
-
-            DialogResult result = MessageBox.Show("Exit?", "DLLtoHEX", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            if (result == DialogResult.Yes)
+            else
             {
-                Application.Exit();
-            }            
+                MessageBox.Show("Smth wrong", "DLLtoHEX");
+            }
         }
     }
 }
